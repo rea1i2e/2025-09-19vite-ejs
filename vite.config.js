6701ba8fa9794d71ec62796041cd7c4fe6c2b291
Yoshiaki Numajiri <afmaar128@gmail.com>
2025-09-20 21:48:01 +0900

    ejs環境ざっくりOK

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,29 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { resolve } from 'node:path';
+import liveReload from "vite-plugin-live-reload";
 
 export default defineConfig({
-  root: './src',
+  root: "./src",
+  server: {
+    open: true, // 自動でブラウザを開く
+  },
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        'index': 'src/index.html',
-        'about': 'src/about/index.html',
-        'scripts/main': 'src/assets/scripts/main.js',
-        'scripts/pages/index/index': 'src/assets/scripts/pages/index/index.js',
-        'scripts/pages/about/index': 'src/assets/scripts/pages/about/index.js',
-        'styles/main': 'src/assets/styles/main.css',
-        'styles/pages/index/index': 'src/assets/styles/pages/index/style.css',
-        'styles/pages/about/index': 'src/assets/styles/pages/about/style.css',
-      },
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+        index: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/about/index.html'),
       },
     },
-  }
+  },
+  plugins: [
+    ViteEjsPlugin({
+      siteName: "ViteEJS検証2025-09-19",
+    }),
+    liveReload([
+      "**/*.ejs"
+    ]),
+  ],
 });
